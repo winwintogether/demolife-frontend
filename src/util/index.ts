bb9ce5b49b0114dff3ee10c4945962dd2be937be
winwintogether <masterinbusiness@outlook.com>
2022-09-15 23:47:42 +0200

    fix: type issue

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,7 +1,7 @@
 export const currency = (num: number | string) => {
   return ('' + num).replace(
     /(\d)(?=(?:\d{3})+(?:\.|$))|(\.\d\d?)\d*$/g,
-    (m, s1, s2) => {
+    (_m, s1, s2) => {
       return s2 || (s1 + ',');
     }
   );
